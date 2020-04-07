99d99b537e475af64b29b04f437905bbc69be7a0
Sundhar <sundhar811@gmail.com>
2020-04-07 16:41:17 +0530

    viewing posts from the database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'Sundhar',
-    'title': 'Blog post 1',
-    'content': 'First post content',
-    'date_posted': 'August 11, 2020'
-  },
-  {
-    'author': 'Doe',
-    'title': 'Blog post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 12, 2020'
-  }
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
 
   return render(request, 'blog/home.html', context)
