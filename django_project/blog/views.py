eb87b04ff9e75289640fe98b0d2c2ec3fdc61a12
Sundhar <sundhar811@gmail.com>
2020-04-07 12:03:08 +0530

    passing data to templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'Sundhar',
+    'title': 'Blog post 1',
+    'content': 'First post content',
+    'date_posted': 'August 11, 2020'
+  },
+  {
+    'author': 'Doe',
+    'title': 'Blog post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 12, 2020'
+  }
+]
+
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
