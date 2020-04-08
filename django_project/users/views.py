b98ecb262c7450b86a9fea7fc7f6c394a8e1e94f
Sundhar <sundhar811@gmail.com>
2020-04-08 12:55:26 +0530

    saving registered user details

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -7,8 +7,11 @@ def register(request):
     form = UserCreationForm(request.POST)
 
     if form.is_valid():
+      form.save() # saving the user in DB
+      
       username = form.cleaned_data.get('username')
       messages.success(request, f"Account created for {username}!")
+      
       return redirect('blog-home')
 
   else:
