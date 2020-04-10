27b4e8c1fa82e98b651f2d70cbde4cf64c01da0f
Sundhar <sundhar811@gmail.com>
2020-04-10 12:14:16 +0530

    post update

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('', views.PostListView.as_view(), name='blog-home'),
   path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
   path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+  path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
   path('about/', views.about, name='blog-about')
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 # def home(request):
@@ -22,6 +22,15 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
   model = Post
   fields = ['title', 'content']
 
