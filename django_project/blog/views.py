6aec89b8c5d327d2867961fab4345c0e3b61b23b
Sundhar <sundhar811@gmail.com>
2020-04-10 12:27:36 +0530

    blog post delete

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+  ListView,
+  DetailView,
+  CreateView,
+  UpdateView,
+  DeleteView
+)
 from .models import Post
 
 # def home(request):
@@ -43,6 +49,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     if self.request.user == post.author:
       return True
     return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+  model = Post
+  success_url = '/'
+
+  def test_func(self):
+    post = self.get_object()
+    return self.request.user == post.author
 
 
 def about(request):
