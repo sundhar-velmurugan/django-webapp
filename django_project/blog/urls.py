9fe8a9b113e8302f3e3ee2bb0a5ce02f42210bef
Sundhar <sundhar811@gmail.com>
2020-04-11 09:37:19 +0530

    blog views refactor

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from . import views
+from .views import (
+  PostListView,
+  UserPostListView,
+  PostDetailView,
+  PostCreateView,
+  PostUpdateView,
+  PostDeleteView,
+  about
+)
 
 urlpatterns = [
-  # path('', views.home, name='blog-home'),
-  path('', views.PostListView.as_view(), name='blog-home'),
-  path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
-  path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-  path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-  path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
-  path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
-  path('about/', views.about, name='blog-about')
+  path('', PostListView.as_view(), name='blog-home'),
+  path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+  path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+  path('post/new/', PostCreateView.as_view(), name='post-create'),
+  path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+  path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+  path('about/', about, name='blog-about')
 ]
