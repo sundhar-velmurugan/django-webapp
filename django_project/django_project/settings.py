03c7854e317302a9c515faf57bdcfb4a21f918bc
Sundhar <sundhar811@gmail.com>
2020-04-12 16:59:47 +0530

    updated settings

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ucd^zs=1mfcacv7*^m_8lsu@t4jbzs*p1oaegmbi9r1m=0pltf'
+# SECRET_KEY = 'ucd^zs=1mfcacv7*^m_8lsu@t4jbzs*p1oaegmbi9r1m=0pltf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-trivia-blog.herokuapp.com']
 
 
 # Application definition
