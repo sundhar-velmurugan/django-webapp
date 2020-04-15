59573bce1630999c44940ce2dbd9dd4b36cab3ab
Sundhar <sundhar811@gmail.com>
2020-04-15 11:09:08 +0530

    removed unwanted imports from settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from botocore.client import Config
-import boto3
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
